baf2c1e56500d66619a977189bc0df9933c49486
tjay13 <harrison_mari@yahoo.com>
2021-10-21 19:20:57 +0200

    Update connection.php

--- Tools/Menu/connection.php
+++ Tools/Menu/connection.php
@@ -2,67 +2,138 @@
 error_reporting(E_ERROR | E_PARSE);
 ini_set('display_errors', '1');
 
-$DB_host = "185.61.137.174";
-$DB_user = "vpnquest1_lucifervpn";
-$DB_pass = "1534Pass1234Five";
-$DB_name = "vpnquest1_lucifervpn";
+$DB_host = '185.61.137.174';
+$DB_user = 'vpnquest1_user';
+$DB_pass = 's+(WT#r4CaB&';
+$DB_name = 'vpnquest1_dbase';
 
 $mysqli = new MySQLi($DB_host,$DB_user,$DB_pass,$DB_name);
 if ($mysqli->connect_error) {
     die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
 }
 
-$data = '';
-#Active Accounts
-$premium_active = "is_validated=1 AND is_active=1 AND frozen=0 AND suspend=1 AND is_duration > 0";
-$vip_active = "is_validated=1 AND is_active=1 AND frozen=0 AND suspend=0 AND vip_duration > 0";
+function encrypt_key($paswd)
+	{
+	  $mykey=getEncryptKey();
+	  $encryptedPassword=encryptPaswd($paswd,$mykey);
+	  return $encryptedPassword;
+	}
+	 
+	// function to get the decrypted user password
+	function decrypt_key($paswd)
+	{
+	  $mykey=getEncryptKey();
+	  $decryptedPassword=decryptPaswd($paswd,$mykey);
+	  return $decryptedPassword;
+	}
+	 
+	function getEncryptKey()
+	{
+		$secret_key = md5('eugcar');
+		$secret_iv = md5('sanchez');
+		$keys = $secret_key . $secret_iv;
+		return encryptor('encrypt', $keys);
+	}
+	function encryptPaswd($string, $key)
+	{
+	  $result = '';
+	  for($i=0; $i<strlen ($string); $i++)
+	  {
+		$char = substr($string, $i, 1);
+		$keychar = substr($key, ($i % strlen($key))-1, 1);
+		$char = chr(ord($char)+ord($keychar));
+		$result.=$char;
+	  }
+		return base64_encode($result);
+	}
+	 
+	function decryptPaswd($string, $key)
+	{
+	  $result = '';
+	  $string = base64_decode($string);
+	  for($i=0; $i<strlen($string); $i++)
+	  {
+		$char = substr($string, $i, 1);
+		$keychar = substr($key, ($i % strlen($key))-1, 1);
+		$char = chr(ord($char)-ord($keychar));
+		$result.=$char;
+	  }
+	 
+		return $result;
+	}
+	
+	function encryptor($action, $string) {
+		$output = false;
 
-$private_active = "is_validated=1 AND is_active=1 AND frozen=0 AND suspend=0 AND private_duration > 0";
+		$encrypt_method = "AES-256-CBC";
+		//pls set your unique hashing key
+		$secret_key = md5('eugcar sanchez');
+		$secret_iv = md5('sanchez eugcar');
+
+		// hash
+		$key = hash('sha256', $secret_key);
+		
+		// iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a warning
+		$iv = substr(hash('sha256', $secret_iv), 0, 16);
+
+		//do the encyption given text/string/number
+		if( $action == 'encrypt' ) {
+			$output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
+			$output = base64_encode($output);
+		}
+		else if( $action == 'decrypt' ){
+			//decrypt the given text/string/number
+			$output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
+		}
+
+		return $output;
+	}
+
+
+
+$data = '';
+$premium = "username AND password AND confirmcode='y' AND status='live' AND is_freeze=1 AND is_active=1 AND is_ban=1 AND is_suspend=1 AND is_duration > 0";
+$vip = "username AND password AND confirmcode='y' AND status='live' AND is_freeze=1 AND is_active=1 AND is_ban=1 AND is_suspend=1 AND vip_duration > 0";
 
 $query = $mysqli->query("SELECT * FROM user
-WHERE ".$vip_active." OR ".$private_active." ORDER by user_id DESC");
+WHERE ".$premium." OR ".$vip."");
 if($query->num_rows > 0)
 {
 	while($row = $query->fetch_assoc())
 	{
 		$data .= '';
 		$username = $row['username'];
-		$password = $row['password'];
-		$data .= 'useradd -p $(openssl passwd -1 '.$password.') -M '.$username.''.PHP_EOL;
+		$password = decrypt_key($row['password']);
+		$password = encryptor('decrypt',$password);		
+		$data .= '/usr/sbin/useradd -p $(openssl passwd -1 '.$password.') -M '.$username.';'.PHP_EOL;
 	}
 }
-$location = '/usr/sbin/kpn/active.sh';
+$location = '/usr/local/bin/kpn/active.sh';
 $fp = fopen($location, 'w');
 fwrite($fp, $data) or die("Unable to open file!");
 fclose($fp);
 
+
 #In-Active and Invalid Accounts
 $data2 = '';
-$premium_deactived = "is_active=1 AND is_duration <= 0";
-$vip_deactived = "is_active=1 AND vip_duration <= 0";
-$private_deactived = "is_active=1 AND private_duration <= 0";
-$is_validated = "is_validated=0";
-$is_activate = "is_active=0";
-$freeze = "frozen=1";
-$suspend = "suspend=1";
+$premium_deactived = "username AND password AND confirmcode='y' AND status='suspended' AND is_freeze=1 AND is_active=0 AND is_ban=1 AND is_suspend=1 AND is_duration <= 0";
+$vip_deactived = "username AND password AND confirmcode='y' AND status='suspended' AND is_freeze=1 AND is_active=0 AND is_ban=1 AND is_suspend=1 AND vip_duration <= 0";
 
 $query2 = $mysqli->query("SELECT * FROM user 
-WHERE ".$is_validated." OR ".$is_activate." OR ".$freeze." OR ".$suspend." OR  ".$premium_deactived ." AND ".$vip_deactived." AND ".$private_deactived."
-");
+WHERE ".$premium_deactived ." OR ".$vip_deactived."");
 if($query2->num_rows > 0)
 {
 	while($row2 = $query2->fetch_assoc())
 	{
 		$data2 .= '';
 		$toadd = $row2['username'];	
-		$data2 .= 'userdel '.$toadd.''.PHP_EOL;
+		$data2 .= '/usr/sbin/userdel '.$toadd.''.PHP_EOL;
 	}
 }
-$location2 = '/usr/sbin/kpn/inactive.sh';
+$location2 = '/usr/local/bin/kpn/inactive.sh';
 $fp = fopen($location2, 'w');
 fwrite($fp, $data2) or die("Unable to open file!");
 fclose($fp);
 
 $mysqli->close();
 ?>
-1
